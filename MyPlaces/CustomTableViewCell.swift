bfe294e817dc0878f62cd1a65fd92a8d8bb28710
Takhir <winzero.nexis@mail.ru>
2020-02-09 22:53:39 +0700

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Zakirov Tahir on 09.02.2020.
+//  Copyright © 2020 Zakirov Takhir. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabek: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
